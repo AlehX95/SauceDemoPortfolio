37642c03493eaa87320bb50b00728f75f564b42d
alehxr <x22180826@student.ncirl.ie>
2025-11-08 11:31:40 +0000

    Test of integration with Jenkins

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Base {
+public class Base { 
 
 	protected WebDriver driver;
 	private WebDriverWait wait;
